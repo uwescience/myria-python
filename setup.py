de39aa7c2aea7ee63b2fd9ae0dd32a2ab0de0fdf
Daniel Halperin <dhalperi@cs.washington.edu>
2014-02-20 17:56:32 -0800

    improve setup.py following http://guide.python-distribute.org/creation.html

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
-from distutils.core import setup
-setup(name='myria-python',
-      version='1.0',
-      packages=['myria'],
-      )
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='myria-python',
+    version='1.0',
+    author='Daniel Halperin',
+    author_email='dhalperi@cs.washington.edu',
+    packages=['myria'],
+    scripts=[],
+    url='https://github.com/uwescience/myria',
+    description='Python interface for Myria.',
+    long_description=open('README.txt').read(),
+    install_requires=["requests",],
+)
