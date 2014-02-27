563dad96f16a74687d96092219c7b2b18fd7a777
Dominik Moritz <domoritz@gmail.com>
2014-02-27 00:07:40 -0800

    Add methods to get profiling logs, allow us to get other things that JSON, also enable streaming for responses

--- myria/connection.py
+++ myria/connection.py
@@ -1,7 +1,10 @@
 import base64
 import ConfigParser
 import json
+import csv
 from time import sleep
+import logging
+
 import requests
 
 from .errors import MyriaError
@@ -10,12 +13,13 @@ __all__ = ['MyriaConnection']
 
 # String constants used in forming requests
 JSON = 'application/json'
+CSV = 'text/plain'
 GET = 'GET'
 PUT = 'PUT'
 POST = 'POST'
 
 # Enable or configure logging
-# logging.basicConfig(level=logging.WARN)
+logging.basicConfig(level=logging.WARN)
 
 
 class MyriaConnection(object):
@@ -68,18 +72,26 @@ class MyriaConnection(object):
         # get workers just to make sure the connection is alive
         self.workers()
 
-    def _finish_async_request(self, method, url, body=None, headers=None):
+    def _finish_async_request(self, method, url, body=None, accept=JSON):
+        headers = {
+            'Accept': accept
+        }
         try:
             while True:
                 if '://' not in url:
                     url = self._url_start + url
-                req = self._session.request(method, url, headers=headers,
-                                            data=body)
-                if req.status_code in [200, 201]:
-                    return req.json()
-                elif req.status_code in [202]:
+                logging.info("Finish async request to {}. Headers: {}".format(
+                    url, headers))
+                r = self._session.request(method, url, headers=headers,
+                                          data=body)
+                if r.status_code in [200, 201]:
+                    if accept == JSON:
+                        return r.json()
+                    else:
+                        return r.text
+                elif r.status_code in [202]:
                     # Get the new URL to poll, etc.
-                    url = req.headers['Location']
+                    url = r.headers['Location']
                     method = GET
                     body = None
                     # Read and ignore the body
@@ -88,28 +100,35 @@ class MyriaConnection(object):
                     sleep(0.1)
                 else:
                     raise MyriaError('Error %d: %s'
-                                     % (req.status_code, req.text))
+                                     % (r.status_code, r.text))
         except Exception as e:
             if isinstance(e, MyriaError):
                 raise
             raise MyriaError(e)
 
-    def _make_request(self, method, url, body=None, headers=None):
+    def _make_request(self, method, url, body=None, accept=JSON):
+        headers = {
+            'Accept': accept
+        }
         try:
             if '://' not in url:
                 url = self._url_start + url
-            response = self._session.request(method, url, headers=headers,
-                                             data=body)
-            if response.status_code in [200, 201, 202]:
-                try:
-                    return response.json()
-                except ValueError, e:
-                    msg = ('Error deserializing JSON: %s. Response: "%s"'
-                           % (e, response.text))
-                    raise MyriaError(msg)
+            logging.info("Make myria request to {}. Headers: {}".format(
+                url, headers))
+            r = self._session.request(method, url, headers=headers,
+                                      data=body, stream=True)
+            if r.status_code in [200, 201, 202]:
+                if accept == JSON:
+                    try:
+                        return r.json()
+                    except ValueError, e:
+                        raise MyriaError(
+                            'Error %d: %s' % (r.status_code, r.text))
+                else:
+                    return r.iter_lines()
             else:
                 raise MyriaError('Error %d: %s'
-                                 % (response.status_code, response.text))
+                                 % (r.status_code, r.text))
         except Exception as e:
             if isinstance(e, MyriaError):
                 raise
@@ -271,6 +290,44 @@ class MyriaConnection(object):
             return fids
         else:
             return []
+
+    def get_sent_logs(self, query_id, fragment_id=None):
+        """Get the logs for where data was sent.
+
+        Args:
+            query_id: the id of a submitted query
+            fragment_id: the id of a fragment
+        """
+        resource_path = '/logs/sent?queryId=%d' % int(query_id)
+        if fragment_id is not None:
+            resource_path += '&fragmentId=%d' % int(fragment_id)
+        response = self._make_request(GET, resource_path, force_json=False)
+        return csv.reader(response)
+
+    def get_profiling_log(self, query_id, fragment_id=None):
+        """Get the logs for operators.
+
+        Args:
+            query_id: the id of a submitted query
+            fragment_id: the id of a fragment
+        """
+        resource_path = '/logs/profiling?queryId=%d' % int(query_id)
+        if fragment_id is not None:
+            resource_path += '&fragmentId=%d' % int(fragment_id)
+        response = self._make_request(GET, resource_path, accept=CSV)
+        return csv.reader(response)
+
+    def get_profiling_log_roots(self, query_id, fragment_id):
+        """Get the logs for root operators.
+
+        Args:
+            query_id: the id of a submitted query
+            fragment_id: the id of a fragment
+        """
+        resource_path = '/logs/profilingroots?queryId=%d&fragmentId=%d' % (int(
+            query_id), int(fragment_id))
+        response = self._make_request(GET, resource_path, accept=CSV)
+        return csv.reader(response)
 
     def queries(self, limit=None, max_=None):
         """Get information about all submitted queries.
