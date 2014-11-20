6ff010eea5c6c3e3434cb513f03c3c3e3add69cb
Daniel Halperin <dhalperi@cs.washington.edu>
2014-11-19 17:17:16 -0800

    bump to 1.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='myria-python',
-    version='1.1.5',
+    version='1.1.6',
     author='Daniel Halperin',
     author_email='dhalperi@cs.washington.edu',
     packages=find_packages(),
