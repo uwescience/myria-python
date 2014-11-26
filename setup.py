ca279000c6089a9e5f473cfd3c61ca2c18323c86
Dominik Moritz <domoritz@gmail.com>
2014-11-25 23:18:01 -0800

    PEP8

--- setup.py
+++ setup.py
@@ -11,8 +11,9 @@ setup(
     description='Python interface for Myria.',
     long_description=open('README.md').read(),
     # see https://stackoverflow.com/questions/18578439
-    install_requires=["pip >= 1.5.6", "pyOpenSSL >= 0.14", "ndg-httpsclient", "pyasn1", "requests",
-        "requests_toolbelt", "messytables", "unicodecsv"],
+    install_requires=["pip >= 1.5.6", "pyOpenSSL >= 0.14", "ndg-httpsclient",
+                      "pyasn1", "requests", "requests_toolbelt",
+                      "messytables", "unicodecsv"],
     entry_points={
         'console_scripts': [
             'myria_upload = myria.cmd.upload_file:main'
