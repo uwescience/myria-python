2ebaa75dd49f8e585ac6415a89af1aec26fed285
Daniel Halperin <dhalperi@cs.washington.edu>
2014-12-15 14:25:47 -0800

    bump to 1.2.0
    Incompatible with 1.1.x

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='myria-python',
-    version='1.1.6',
+    version='1.2.0',
     author='Daniel Halperin',
     author_email='dhalperi@cs.washington.edu',
     packages=find_packages(),
