c4ca55fc2174a7d6c8dc0cb0cf923c349f7766d9
Brandon Haynes <bhaynes@cs.washington.edu>
2015-02-24 11:16:35 -0800

    Moving around a bracket

--- myria/plans.py
+++ myria/plans.py
@@ -52,8 +52,7 @@ def _get_parallel_import_fragment(taskid, schema, relation,
                      "argOverwriteTable": True,
 
                      "relationKey": relation
-                     }.items() + (insert_metadata or {}).items())
-              ]}
+                     }.items() + (insert_metadata or {}).items())]}
 
 
 def __increment(value):
