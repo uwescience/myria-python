2760726810d036cffd991c59345ebdc17576c97f
Brandon Haynes <bhaynes@cs.washington.edu>
2015-03-19 11:11:16 -0700

    Added some defensive constructor argument checks in MyriaSchema

--- myria/schema.py
+++ myria/schema.py
@@ -1,10 +1,22 @@
 """ Higher-level types for interacting with Myria schema """
 
+SCHEMA_TYPES = ['INT_TYPE', 'FLOAT_TYPE', 'DOUBLE_TYPE', 'BOOLEAN_TYPE',
+                'STRING_TYPE', 'LONG_TYPE', 'DATETIME_TYPE']
+
 
 class MyriaSchema(object):
     """ Represents a schema for a Myria relation """
 
     def __init__(self, json):
+        if len(json['columnNames']) == 0:
+            raise ValueError('Schema must have at least one attribute.')
+        elif len(json['columnNames']) != len(json['columnTypes']):
+            raise ValueError('Schema must have the same number of attributes '
+                             'and types.')
+        elif any((value not in SCHEMA_TYPES for value in json['columnTypes'])):
+            raise ValueError('One or more schema of the following types are '
+                             'invalid: ' + ', '.join(json['columnTypes']))
+
         self.json = json
         self.names = json['columnNames']
         self.types = json['columnTypes']
