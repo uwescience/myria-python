e3c1c8b0513d8ab45a7ba66540a86d925e3d0bc9
Brandon Haynes <bhaynes@cs.washington.edu>
2015-03-21 11:07:18 -0700

    Exposed scan/insert types and parameters in MyriaQuery.parallel_import

--- myria/plans.py
+++ myria/plans.py
@@ -3,9 +3,13 @@
 from functools import partial
 
 
+DEFAULT_SCAN_TYPE = 'FileScan'
+DEFAULT_INSERT_TYPE = 'DbInsert'
+
+
 def get_parallel_import_plan(schema, work, relation, text='',
-                             scan_metadata=None, insert_metadata=None,
-                             scan_type='FileScan', insert_type='DbInsert'):
+                             scan_parameters=None, insert_parameters=None,
+                             scan_type=None, insert_type=None):
     """ Generate a valid JSON Myria plan for parallel import of data
 
     work: list of (worker-id, data-source) pairs; data-source should be a
@@ -14,8 +18,8 @@ def get_parallel_import_plan(schema, work, relation, text='',
 
     Keyword arguments:
       text: description of the plan
-      scan_metadata: dict of additional operator parameters for the scan
-      insert_metadata: dict of additional operator parameters for the insertion
+      scan_parameters: dict of additional operator parameters for the scan
+      insert_parameters: dict of additional operator parameters for insertion
       scan_type: type of scan to perform
       insert_Type: type of insert to perform
     """
@@ -23,14 +27,14 @@ def get_parallel_import_plan(schema, work, relation, text='',
         {"fragments": map(partial(_get_parallel_import_fragment, [0],
                                   schema, relation,
                                   scan_type, insert_type,
-                                  scan_metadata, insert_metadata), work),
+                                  scan_parameters, insert_parameters), work),
          "logicalRa": text,
          "rawQuery": text}
 
 
 def _get_parallel_import_fragment(taskid, schema, relation,
                                   scan_type, insert_type,
-                                  scan_metadata, insert_metadata,
+                                  scan_parameters, insert_parameters,
                                   assignment):
     """ Generate a single fragment of the parallel import plan """
     worker_id = assignment[0]
@@ -39,20 +43,20 @@ def _get_parallel_import_fragment(taskid, schema, relation,
             "operators": [
                 dict({
                      "opId": __increment(taskid),
-                     "opType": scan_type,
+                     "opType": scan_type or DEFAULT_SCAN_TYPE,
 
                      "schema": schema.to_json(),
                      "source": datasource
-                     }.items() + (scan_metadata or {}).items()),
+                     }.items() + (scan_parameters or {}).items()),
                 dict({
                      "opId": __increment(taskid),
-                     "opType": insert_type,
+                     "opType": insert_type or DEFAULT_INSERT_TYPE,
 
                      "argChild": taskid[0] - 2,
                      "argOverwriteTable": True,
 
                      "relationKey": relation
-                     }.items() + (insert_metadata or {}).items())]}
+                     }.items() + (insert_parameters or {}).items())]}
 
 
 def __increment(value):
