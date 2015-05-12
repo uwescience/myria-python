b9602cd7af5c13554299d32ba2aa712269eb0060
Brandon Haynes <BrandonHaynes@users.noreply.github.com>
2015-05-11 17:33:43 -0700

    Bump to version 1.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='myria-python',
-    version='1.2.2',
+    version='1.2.3',
     author='Daniel Halperin',
     author_email='dhalperi@cs.washington.edu',
     packages=find_packages(),
