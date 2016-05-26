34650868d24f6d6627ee555c09d8e45be82625da
Parmita <parmitam@hotmail.com>
2016-05-26 16:30:42 -0700

    adding support for bytes type and ability to register and list UDFs

---
+++ myria/udf.py
@@ -0,0 +1,39 @@
+"""User Defined functions"""
+import json
+from .errors import MyriaError
+import cloud
+import base64
+
+
+class functionTypes(object):
+  POSTGRES = 0
+  PYTHON = 1
+
+def create_function(name,text, outSchema,inSchema,lang,binary=None):
+
+    body = None
+
+    if(lang==functionTypes.POSTGRES):
+        body = {'name': name,
+                'text': text,
+                'outputSchema': outSchema.to_dict(),
+                'inputSchema':inSchema.to_dict(),
+                'lang': functionTypes.POSTGRES}
+    elif(lang==functionTypes.PYTHON):
+        if(binary==None or inSchema==None):
+            raise MyriaError("Cannot create a python function without binary or input schema.")
+        else :
+            obj = cloud.serialization.cloudpickle.dumps(binary, 2)
+            bo = base64.b64encode(obj)
+            body = {'name':name,
+                    'text':text,
+                    'outputSchema':outSchema.to_dict(),
+                    'inputSchema':inSchema.to_dict(),
+                    'lang':functionTypes.PYTHON,
+                    'binary': bo }
+
+    if (body==None):
+        raise MyriaError("Unsupported language for user function.")
+    print "Body of message"
+    print body
+    return body
