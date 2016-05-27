ef3bc5a7c826f98f580ecec1e35bf29dbbb98e69
jingjingwang <jwang@cs.washington.edu>
2016-05-27 15:44:40 -0700

    Stub for function decorator

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     # see https://stackoverflow.com/questions/18578439
     install_requires=["pip >= 1.5.6", "pyOpenSSL >= 0.14", "ndg-httpsclient",
                       "pyasn1", "requests", "requests_toolbelt",
-                      "messytables", "unicodecsv"],
+                      "messytables", "unicodecsv", "raco >= 1.2.0"],
     entry_points={
         'console_scripts': [
             'myria_upload = myria.cmd.upload_file:main'
