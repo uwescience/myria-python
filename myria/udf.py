0ae4c55864babb68c0513d00c116a31ee230e805
Parmita <parmitam@hotmail.com>
2016-06-16 10:54:32 -0700

    changes

--- myria/udf.py
+++ myria/udf.py
@@ -24,7 +24,7 @@ def create_function(name,text, outSchema,inSchema,lang,binary=None):
             raise MyriaError("Cannot create a python function without binary or input schema.")
         else :
             obj = cloud.serialization.cloudpickle.dumps(binary, 2)
-            bo = base64.b64encode(obj)
+            bo = base64.urlsafe_b64encode(obj)
             body = {'name':name,
                     'text':text,
                     'outputSchema':outSchema.to_dict(),
