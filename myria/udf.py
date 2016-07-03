388361e0e8e043db7641540637c5adedd788e2d1
Parmita <parmitam@hotmail.com>
2016-07-03 11:05:53 -0700

    fix function registration endpoint

--- myria/udf.py
+++ myria/udf.py
@@ -9,24 +9,24 @@ class functionTypes(object):
   POSTGRES = 0
   PYTHON = 1
 
-def create_function(name,text, outSchema,inSchema,lang,binary=None):
+def create_function(name,text, inSchema, outType,lang,binary=None):
     body = None
 
     if(lang==functionTypes.POSTGRES):
         body = {'name': name,
                 'text': text,
-                'outputSchema': outSchema.to_dict(),
+                'outputType': outType,
                 'inputSchema':inSchema.to_dict(),
                 'lang': functionTypes.POSTGRES}
     elif(lang==functionTypes.PYTHON):
-        if(binary==None or outSchema==None):
-            raise MyriaError("Cannot create a python function without binary or input schema.")
+        if(binary==None or outType==None):
+            raise MyriaError("Cannot create a python function without binary or output type .")
         else :
             obj = cloud.serialization.cloudpickle.dumps(binary, 2)
             bo = base64.urlsafe_b64encode(obj)
             body = {'name':name,
                     'text':text,
-                    'outputSchema':outSchema.to_dict(),
+                    'outputType':outType,
                     'inputSchema':inSchema.to_dict(),
                     'lang':functionTypes.PYTHON,
                     'binary': bo }
