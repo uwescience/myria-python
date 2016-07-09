230554c3c571417bd90fbcb02faf5c02973e183b
Parmita <parmitam@hotmail.com>
2016-07-09 14:24:25 -0700

    fixing cloud pickle issues

--- myria/udf.py
+++ myria/udf.py
@@ -1,33 +1,38 @@
 """Creating User Defined functions"""
 import json
 from .errors import MyriaError
-from .cloudpickle import *
+from myria  import cloudpickle
 import base64
 
 class functionTypes(object):
   POSTGRES = 0
   PYTHON = 1
 
-def create_function(name,text, inSchema, outType,lang,binary=None):
+def create_function(name, text, outType, lang, inSchema = None, binary=None):
     body = None
 
-    if(lang==functionTypes.POSTGRES):
+    if(inSchema is None or inSchema == ""):
+        inputSchema = ""
+    else:
+        inputschema = inSchema.to_dict()
+
+    if(lang == functionTypes.POSTGRES):
         body = {'name': name,
                 'text': text,
                 'outputType': outType,
-                'inputSchema':inSchema.to_dict(),
+                'inputSchema': inputschema,
                 'lang': functionTypes.POSTGRES}
-    elif(lang==functionTypes.PYTHON):
-        if(binary==None or outType==None):
+    elif(lang == functionTypes.PYTHON):
+        if(binary is None or outType is None):
             raise MyriaError("Cannot create a python function without binary or output type .")
         else :
-            obj = cloudpickler.dumps(binary, 2)
+            obj = cloudpickle.dumps(binary, 2)
             bo = base64.urlsafe_b64encode(obj)
-            body = {'name':name,
-                    'text':text,
-                    'outputType':outType,
-                    'inputSchema':inSchema.to_dict(),
-                    'lang':functionTypes.PYTHON,
+            body = {'name': name,
+                    'text': text,
+                    'outputType': outType,
+                    'inputSchema': inputschema,
+                    'lang': functionTypes.PYTHON,
                     'binary': bo }
 
     if (body==None):
