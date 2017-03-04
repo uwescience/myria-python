78dccaf0cf6e8d80f7cb082e72a2866c8ed48a43
Brandon Haynes <bhaynes@cs.washington.edu>
2017-03-03 16:06:38 -0800

    Bump version for kill query support

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='myria-python',
     #namespace_packages = ['myria'],
-    version='1.2.6',
+    version='1.2.7',
     author='Daniel Halperin',
     author_email='dhalperi@cs.washington.edu',
     packages=find_packages(),
