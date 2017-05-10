bced1bab6719ff9b4a674657e1ca7c88b9f5c2e7
Parmita <parmitam@hotmail.com>
2017-05-09 19:12:00 -0700

    removing merge comment

--- setup.py
+++ setup.py
@@ -12,12 +12,7 @@ setup(
     description='Python interface for Myria.',
     long_description=open('README.md').read(),
     setup_requires=["requests>=2.5.1"],
-<<<<<<< HEAD
     # see https://stackoverflow.com/questions/18578439
-    install_requires=["pip >= 1.5.6", "pyOpenSSL >= 0.14", "ndg-httpsclient",
-                      "messytables", "unicodecsv", "raco >= 1.3.5",
-                      "python-dateutil"],
-=======
     install_requires=["pip >= 1.5.6", "pyOpenSSL >= 0.14", "ndg-httpsclient",
                       "pyasn1", "requests", "requests_toolbelt",
                       "messytables", "unicodecsv", "raco >= 1.3.5",
